3c4bf67f20f44c5de1c8f8c4d7c15d2c9b2a6c58
vradicevic <47304793+vradicevic@users.noreply.github.com>
2020-06-06 23:15:38 +0200

    Update env.php

--- env.php
+++ env.php
@@ -3,8 +3,8 @@ namespace db;
 
 class DbConfig
 {
-    const HOST = "localhost";
-    const USER = "root";
-    const PASS = "";
-    const DB = "cfc";
+    const HOST = "eu-cdbr-west-03.cleardb.net";
+    const USER = "b2328665af975b";
+    const PASS = "04100033";
+    const DB = "heroku_dd677f260c343e6";
 }
